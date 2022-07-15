5cea3439118a023cbbe836f08f29ee6f03b61e1e
VeerayyaSS <108250703+VeerayyaSS@users.noreply.github.com>
2022-07-15 09:23:47 +0530

    Add files via upload

---
+++ multi1/inc/multi.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include <sys/wait.h>
+
+#include <fstream>
+
+#include <string>
+
+#include <cstring>
+
+#include <vector>
+
+#include <stdio.h>
+
+#define PATH "/home/codest/work/SysPrg/multi1/"
+
+using namespace std;
---
+++ multi1/src/assgn1.cpp
@@ -0,0 +1,59 @@
+#include "multi.h"
+#include<iostream>
+#include<fstream>
+
+int main(int argc, char* argv[])
+{
+	fstream fs;
+    int pid,status;
+    string buff=argv[1];
+    string fpath = PATH;
+    fpath +=buff;
+    pid=fork();
+    if(pid==0){
+        cout<<"Child Process ID "<<getpid()<<endl;
+        cout<<"Enter the lines into the file"<<endl;
+        vector<string> lines;
+        string line;
+        for (int i = 0; i < 2; i++) {
+            getline(cin, line);
+            lines.push_back(line);
+        }
+        
+        fstream fs;
+	    fs.open(fpath,ios::in | ios::out | ios::binary);
+	    if(!fs)
+	    {
+		    cout<<"Unable to open the file"<<endl;
+		    exit(0);
+	    }
+        
+	    size_t size = lines.size();
+        fs.write(reinterpret_cast<const char*>(&lines), size*sizeof(string));
+    
+	    fs.close();
+    }
+	else if (WIFEXITED(status)) {
+        
+        wait((int *)0);
+        cout<<"Parent Process ID "<<getpid()<<endl;
+        string myline;
+        vector<string> lines;
+        string line;
+        fstream f;
+	    f.open(fpath,ios::in | ios::out | ios::binary);
+	    if(!f)
+	    {
+		    cout<<"Unable to open file"<<endl;
+		    exit(0);
+	    }
+         size_t sizes = lines.size();
+        f.read(reinterpret_cast<char*>(&lines), sizes*sizeof(string));
+        
+        f.close();
+
+    }
+    else
+
+	return 0;
+}
